576b006ff7e6dc7bd406ac7cfc6ebeaf15016142
Michael Phillips <80991810+mphillips01@users.noreply.github.com>
2022-06-27 15:37:24 -0400

    fixing rsa

--- asymmetricEncryption.py
+++ asymmetricEncryption.py
@@ -1,21 +1,22 @@
+from base64 import b64encode
 import rsa
 
 # create function to create rsa key pair
 
 def createRsaKeyPair():
     # create key pair
-    key = rsa.generate(2048)
+    (publicKey, privateKey) = rsa.newkeys(2048)
     # create public and private key files
     publicKeyFile = open("rsaPublicKey.txt", "wb")
     privateKeyFile = open("rsaPrivateKey.txt", "wb")
     # write public key to public key file
-    publicKeyFile.write(key.publickey().exportKey("PEM"))
+    publicKeyFile.write(publicKey.save_pkcs1())
     # write private key to private key file
-    privateKeyFile.write(key.exportKey("PEM"))
+    privateKeyFile.write(privateKey.save_pkcs1())
     # close files
     publicKeyFile.close()
     privateKeyFile.close()
-    return key
+    return (publicKey, privateKey)
 
 #read rsa public key
 def readRsaPublicKey():
@@ -39,13 +40,15 @@ def rsaEncrypt(key):
     fernetKeyFile.close()
     #copy fernet key and save it to rsa_sym_key_plain.txt
     fernetKeyFile = open("rsa_sym_key_plain.txt", "w")
+    fernetKeyString = b64encode(key).decode('utf-8')
     fernetKeyFile.write(fernetKeyString)
     fernetKeyFile.close()
     # encrypt fernet key using public key
     encryptedFernetKey = rsa.encrypt(fernetKeyString, key)
     # write encrypted fernet key to file
-    encryptedFernetKeyFile = open("rsa_sym_key_cipher.txt", "wb")
-    encryptedFernetKeyFile.write(encryptedFernetKey)
+    encryptedFernetKeyFile = open("rsa_sym_key_cipher.txt", "w")
+    encryptedFernetKeyString = b64encode(encryptedFernetKey).decode('utf-8')
+    encryptedFernetKeyFile.write(encryptedFernetKeyString)
     encryptedFernetKeyFile.close()
     return encryptedFernetKey
 
